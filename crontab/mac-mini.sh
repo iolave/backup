1d46ef01f6616cdde7ca12b54b750f9be179b263
Ignacio Olave <me@iolave.com>
2025-09-15 10:24:09 -0300

    refactor: script now backs up mfh records folder instead of cubase projects one

--- crontab/mac-mini.sh
+++ crontab/mac-mini.sh
@@ -11,18 +11,24 @@
 DATE="$(date +\%Y-\%m-\%d_\%H-\%M)"
 
 mkdir -p $HOME/Library/Logs/rsync
+RSYNC_LOG_PATH="$HOME/Library/Logs/rsync"
 
 # Backup photos library
 /opt/homebrew/bin/rsync -avP --delete \
 	--password-file=$HOME/.rsync/nas.internal.pingolabs.com.password \
-	--log-file="$HOME/Library/Logs/rsync/pictures-$DATE.log" \
+	--log-file="$RSYNC_LOG_PATH/pictures-$DATE.log" \
 	$HOME/Pictures \
 	rsync@nas.internal.pingolabs.cl::Ignacio\ Olave/
 
-# backup cubase project files
+# backup MFH Records project files
+#
+# Includes:
+#   - cubase project files
+#   - graphic design work
+#   - music related work
 /opt/homebrew/bin/rsync -avP \
 	--password-file=$HOME/.rsync/nas.internal.pingolabs.com.password \
-	--log-file="$HOME/Library/Logs/rsync/cubase-$DATE.log" \
-	$HOME/Music/cubase \
+	--log-file="$RSYNC_LOG_PATH/mfh-records-$DATE.log" \
+	$HOME/../../MFH\ Records \
 	rsync@nas.internal.pingolabs.cl::Ignacio\ Olave/Backup/
 
